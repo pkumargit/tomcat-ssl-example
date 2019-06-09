ed04c918e3b28ddf967cf4eabe528a3eef9b6dd5
pkumargit <pankaj.kumar@marklogic.com>
2019-06-09 12:55:05 +0530

    jenkinfile groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,17 @@
+node {
+  stage('Checkout SCM') {
+    checkout scm
+  }
+  stage('Build Dockerfile') {
+      sh "/usr/local/bin/docker login -u 'pkuma343' -p 'Ponkmonk_138202' 'https://hub.docker.com'"
+      sh "/usr/local/bin/docker build -t 'pkuma343/tomcat-ssl-example:develop' ."
+      sh "/usr/local/bin/docker push 'pkuma343/tomcat-ssl-example:develop'"
+    }
+  stage('Deploy to Openshift') {
+    sh "/usr/local/bin/oc login 'https://console.starter-us-east-1.openshift.com:443' -u 'pkuma343' -p 'Ponkmonk_138202' --insecure-skip-tls-verify"
+    sh "/usr/local/bin/oc new-project 'tomcat-ssl-example'"
+    sh "/usr/local/bin/oc project 'tomcat-ssl-example'"
+    // sh "/usr/local/bin/oc create -f apidemo.yaml"
+    sh "/usr/local/bin/oc new-app --docker-image='pkuma343/tomcat-ssl-example:develop'"
+  }
+}
