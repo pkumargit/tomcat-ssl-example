777704d73198ef83364d39252b807adb59101a88
Pankaj Kumar (C360-EPMP) <pkuma343@LAMU02X25SKJG5M.uhc.com>
2019-06-09 12:24:01 +0530

    initial commit

---
+++ src/main/java/com/tomcat/ssl/SampleTomcatSslApplication.java
@@ -0,0 +1,11 @@
+package com.tomcat.ssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleTomcatSslApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SampleTomcatSslApplication.class, args);
+	}
+}
